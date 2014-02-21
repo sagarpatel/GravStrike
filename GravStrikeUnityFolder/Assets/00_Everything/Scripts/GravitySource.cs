2f7fd4340ce409250cd7d59c51018bee751a09e3
Sagar Patel <17.sagar.patel@gmail.com>
2014-02-21 11:49:26 +0900

    Added basic GravitySource code, wip

---
+++ GravStrikeUnityFolder/Assets/00_Everything/Scripts/GravitySource.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravitySource : MonoBehaviour 
+{
+
+	[Range(1,10)]
+	public float gravityReach = 2.0f;
+	public enum rotationTypes
+	{
+		ClockWise, CounterClockWise
+	}
+	public rotationTypes gravityRotation;
+
+	GameObject[] gameObjectsInReach;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetGameObjectsInReach();
+	}
+
+	void GetGameObjectsInReach()
+	{
+		HiggsBoson[] goHBArray = GameObject.FindObjectsOfType(typeof(HiggsBoson)) as HiggsBoson[] ;
+		//Debug.Log(goHBArray);
+		for(int i = 0; i< goHBArray.Length; i ++)
+		{
+			GameObject higgBosonGO = goHBArray[i].transform.gameObject;
+			//Debug.Log(higgBosonGO);
+		}
+
+
+	}
+
+	void OnDrawGizmosSelected() 
+	{
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, gravityReach);
+    }
+
+
+}
