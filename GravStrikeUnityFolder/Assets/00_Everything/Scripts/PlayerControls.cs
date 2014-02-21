6bf337d13a14daf7ff0d81d3526ad3ac2db7b40d
Sagar Patel <17.sagar.patel@gmail.com>
2014-02-21 17:22:35 +0900

    First InControl input test: WORKS

--- GravStrikeUnityFolder/Assets/00_Everything/Scripts/PlayerControls.cs
+++ GravStrikeUnityFolder/Assets/00_Everything/Scripts/PlayerControls.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
-public class PlayerControls : MonoBehaviour {
+public class PlayerControls : MonoBehaviour 
+{	
+	public int playerIndex = 0;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		var inputDevice = InputManager.Devices[playerIndex];
+
+		if(inputDevice.Action1.IsPressed)
+		{
+			Debug.Log("Button Pressed!");
+		}
 	}
+
+
 }
