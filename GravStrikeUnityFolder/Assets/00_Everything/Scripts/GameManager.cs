7b9c56d21aaf51e61c684af6137e52022539528e
Sagar Patel <17.sagar.patel@gmail.com>
2014-02-21 17:11:57 +0900

    Starting to do use InControl for input

---
+++ GravStrikeUnityFolder/Assets/00_Everything/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class GameManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		InputManager.Setup();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		InputManager.Update();
+
+	}
+
+
+}
