0ad7a525dd503953ad953fdb0917380fbfd28b10
Steven Edwon <edwon@me.com>
2014-02-21 17:50:41 +0900

    working on player prefab and gun script

--- GravStrikeUnityFolder/Assets/00_Everything/Scripts/Gun.cs
+++ GravStrikeUnityFolder/Assets/00_Everything/Scripts/Gun.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Gun : MonoBehaviour {
 
+	private 
+
 	void Start () 
 	{
 		Shoot ();
@@ -10,11 +12,13 @@ public class Gun : MonoBehaviour {
 	
 	void Update () 
 	{
-	
+		
 	}
 
 	void Shoot ()
 	{
-		
+		GameObject bullet = (GameObject)Instantiate(Resources.Load("BulletPrefab"));
+		bullet.transform.position = transform.position;
+		bullet.transform.rotation = transform.rotation;
 	}
 }
