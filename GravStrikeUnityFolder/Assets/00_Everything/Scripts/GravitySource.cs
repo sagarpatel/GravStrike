4a1e20609c3687f6ddf0ec44946bf28f31d68691
Sagar Patel <17.sagar.patel@gmail.com>
2014-02-22 16:17:18 +0900

    Tested multiple gravity sources, MUCH more fun to move

--- GravStrikeUnityFolder/Assets/00_Everything/Scripts/GravitySource.cs
+++ GravStrikeUnityFolder/Assets/00_Everything/Scripts/GravitySource.cs
@@ -100,7 +100,7 @@ public class GravitySource : MonoBehaviour
 
 	}
 
-	void OnDrawGizmosSelected() 
+	void OnDrawGizmos() 
 	{
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, gravityReach);
