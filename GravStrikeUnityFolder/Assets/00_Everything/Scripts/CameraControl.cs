850fc4130f88d67d0bab06347f94713bca8034b3
Steven Edwon <edwon@me.com>
2014-02-25 16:29:12 +0900

    working on camera control script

---
+++ GravStrikeUnityFolder/Assets/00_Everything/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	GameObject[] players;
+	public float camSizeAdjust;
+
+	void Start () 
+	{
+		players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	void Update () 
+	{
+		Bounds box = new Bounds(gameObject.transform.position, new Vector3(1,1,1));
+		foreach (GameObject p in players)
+		{
+			box.Encapsulate(p.transform.position);
+		}
+		camera.transform.position = new Vector3(box.center.x, box.center.y, -100);
+//		camera.orthographicSize = box.size.x / camSizeAdjust;
+
+	}
+}
